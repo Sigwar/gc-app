2905da627ea4cd6aeeb9c4b06452303be0430b54
Rafał Krukowski <kruku13@gmail.com>
2020-08-14 19:30:32 +0200

    SETUP PROJECT

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/images/logo.png">
+    <el-button type="warning">WARNING</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
